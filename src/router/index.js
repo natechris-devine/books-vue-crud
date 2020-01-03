5b95f22f8bbef199811185025ed6a202e2868c85
NateChris <divineconclusion@gmail.com>
2020-01-03 14:09:59 -0500

    Completed Vue section of CRUD app Books

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Books from '../components/Books.vue';
+import Ping from '../components/Ping.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'Books',
+    component: Books,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/ping',
+    name: 'Ping',
+    component: Ping,
   },
 ];
 
